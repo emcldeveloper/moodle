c45948c8ed19b08ca5387044726b2c92e2276821
mushycosma <kevomassawe@gmail.com>
2025-11-14 14:09:28 +0300

    moddle

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '';
+$CFG->prefix    = 'mdl_';
+
+$CFG->wwwroot   = 'http://moodle.local';
+$CFG->dirroot   = 'C:\\xampp\\htdocs\\moodle';
+$CFG->dataroot  = 'C:\\xampp\\moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
